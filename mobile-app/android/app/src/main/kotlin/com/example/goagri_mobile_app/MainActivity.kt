5df9ca91f7b2240a731637697784a8c73b09a083
tahamie <108269079+tahamie@users.noreply.github.com>
2026-07-28 15:24:27 +0500

    Untrack mobile app source from web git repository for fast Vercel deployment

--- mobile-app/android/app/src/main/kotlin/com/example/goagri_mobile_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.goagri_mobile_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile-app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:goagri_mobile_app/main.dart';
-
-void main() {
-  testWidgets('App renders test', (WidgetTester tester) async {
-    await tester.pumpWidget(const GoAgriMobileApp());
-    expect(find.byType(GoAgriMobileApp), findsOneWidget);
-  });
-}
