a5d2d4fffb0289d3f53aa46a40ca5def7b9cf39c
tahamie <108269079+tahamie@users.noreply.github.com>
2026-08-04 18:05:05 +0500

    Update default PORT to 3020 for CloudRevol web host

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ const rateRoutes = require('./routes/rates');
 const bankRoutes = require('./routes/banks');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3020;
 
 app.use(cors());
 app.use(express.json());
